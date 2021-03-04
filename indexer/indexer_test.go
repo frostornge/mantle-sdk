b80f8a108c90198b02e3ed9fe25d961bc777dab6
frostornge <frostornge@gmail.com>
2021-03-04 17:24:43 +0900

    test(indexer): add test structs

---
+++ indexer/indexer_test.go
@@ -0,0 +1,108 @@
+package indexer
+
+type TestQuery struct {
+	Foo string
+	Bar struct {
+		Hello  uint64
+		Mantle string
+	}
+}
+
+// single
+var _ Indexer = TestIndexer{}
+
+type TestIndexer struct {
+	Foo string
+	Bar struct {
+		Hello  uint64 `model:"index"`
+		Mantle string `model:"index"`
+	}
+}
+
+func (i TestIndexer) Name() string { return "" }
+func (i TestIndexer) Index(q Querier, c Committer) error {
+	req := TestQuery{}
+	if err := q(&req, nil); err != nil {
+		return err
+	}
+
+	entity := TestIndexer{
+		Foo: req.Foo,
+		Bar: struct {
+			Hello  uint64 `model:"index"`
+			Mantle string `model:"index"`
+		}{
+			Hello:  req.Bar.Hello,
+			Mantle: req.Bar.Mantle,
+		},
+	}
+	if err := c(entity); err != nil {
+		return err
+	}
+	return nil
+}
+
+// slice
+var _ Indexer = TestSliceIndexer{}
+
+type TestSliceIndexer []TestIndexer
+
+func (i TestSliceIndexer) Name() string { return "" }
+func (i TestSliceIndexer) Index(q Querier, c Committer) error {
+	var req []TestQuery
+	if err := q(&req, nil); err != nil {
+		return err
+	}
+
+	var entity TestSliceIndexer
+	for _, r := range req {
+		entity = append(
+			entity,
+			TestIndexer{
+				Foo: r.Foo,
+				Bar: struct {
+					Hello  uint64 `model:"index"`
+					Mantle string `model:"index"`
+				}{
+					Hello:  r.Bar.Hello,
+					Mantle: r.Bar.Mantle,
+				},
+			},
+		)
+	}
+	if err := c(entity); err != nil {
+		return err
+	}
+	return nil
+}
+
+// map
+var _ Indexer = TestMapIndexer{}
+
+type TestMapIndexer map[string]TestIndexer
+
+func (i TestMapIndexer) Name() string { return "" }
+func (i TestMapIndexer) Index(q Querier, c Committer) error {
+	var req map[string]TestQuery
+	if err := q(&req, nil); err != nil {
+		return err
+	}
+
+	entity := make(TestMapIndexer)
+	for k, r := range req {
+		entity[k] = TestIndexer{
+			Foo: r.Foo,
+			Bar: struct {
+				Hello  uint64 `model:"index"`
+				Mantle string `model:"index"`
+			}{
+				Hello:  r.Bar.Hello,
+				Mantle: r.Bar.Mantle,
+			},
+		}
+	}
+	if err := c(entity); err != nil {
+		return err
+	}
+	return nil
+}
